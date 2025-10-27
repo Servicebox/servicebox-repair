fea54206b398f3a51d78bcc9f8e80cae4bb5a8f0
tom <tom@MacBook-Pro-tom.local>
2025-10-27 20:44:02 +0300

    slash

--- src/app/api/upload.js
+++ src/app/api/upload.js
@@ -1,12 +1,12 @@
 export const config = {
   api: {
     bodyParser: {
-      sizeLimit: '1mb', // You can adjust the size as needed
+      sizeLimit: '1mb',
     },
   },
 };
 
-// Your API handler function below
+
 export default function handler(req, res) {
-  // ... your API logic
+
 }
